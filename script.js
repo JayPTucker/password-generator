7d8b1727912195af28b5be1ab83df7afa6ad8874
JayPTucker <jayptuckerimportant@gmail.com>
2020-02-18 15:08:04 -0600

    Got the password to appear in the input box

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // DOM ELEMENTS
 const Characters = document.getElementById('length');
-const generateEl = document.getElementById('generate');
+let generateEl = document.getElementById('generate-btn');
 const wantLower = document.getElementById('lowercase');
 const wantUpper = document.getElementById('uppercase');
 const wantNumbers = document.getElementById('numbers');
@@ -10,8 +10,9 @@ const password = document.getElementById('passwordresult');
 // GENERATOR ITEMS
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "{", "}", "[", "]", ",", "."];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "{", "}", "[", "]", ",", "."]
+var results = "";
 
 
 // THE GENERATOR
@@ -32,10 +33,11 @@ generateEl.addEventListener('click', () => {
     }
 
     for (var i = 0; i < Characters.value; i++) {
-        document.write(all[Math.floor(Math.random() * all.length)]);
+        results += (all[Math.floor(Math.random() * all.length)]);
     }
-}) 
 
+    document.getElementById("passwordresult").value = results;
+});
 
-
-
+// generateEl.addEventListener("click", myFunction);
+// generateEl.addEventListener("click", coolFunction);
